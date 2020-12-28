072c5c94d351252638d667e7b877680cf37a453e
dhanlaxmihackosphere <74894198+dhanlaxmihackosphere@users.noreply.github.com>
2020-12-28 15:13:16 +0530

    edited -y

--- php.sh
+++ php.sh
@@ -12,4 +12,4 @@ sudo apt-get -y install php7.0-mysql php7.0-curl php7.0-json php7.0-cgi php7.0-x
 
 apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-mbstring php7.0-gd php7.0-xml php7.0-xmlrpc php7.0-intl php7.0-soap php7.0-zip
 
-sudo apt install php libapache2-mod-php php-mysql
+sudo apt-get -y install php libapache2-mod-php php-mysql
