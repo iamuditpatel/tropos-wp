f8cc9639d58baefe03f04140f9ac66ae8a8e2449
dhanlaxmihackosphere <74894198+dhanlaxmihackosphere@users.noreply.github.com>
2020-12-28 15:11:12 +0530

    remove ufw status

--- firewall.sh
+++ firewall.sh
@@ -7,11 +7,7 @@ sudo ufw app list
 #To allow Apache ufw
 sudo ufw allow in "Apache"
 
-#To check status of ufw
-sudo ufw status
-
 #If Ufw is disable then to enable it
 sudo ufw enable
 
-#Again To check status of ufw
-sudo ufw status
+
