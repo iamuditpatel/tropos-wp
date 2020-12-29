aaafc0c5909040f53aed0f8e441563b4acc0c027
dhanlaxmihackosphere <74894198+dhanlaxmihackosphere@users.noreply.github.com>
2020-12-29 14:19:00 +0530

    edited

--- php.sh
+++ php.sh
@@ -4,10 +4,10 @@
 sudo apt-get -y update && sudo apt-get -y install php libapache2-mod-php php-mysql
 
 #PHP MyAdmin Installation
-sudo apt-get -y update && sudo apt-get -y install phpmyadmin php-mbstring
+#sudo apt-get -y update && sudo apt-get -y install phpmyadmin php-mbstring
 
-sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
+#sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
 
-sudo a2enconf phpmyadmin.conf
+#sudo a2enconf phpmyadmin.conf
 
-sudo service apache2 reload
+#sudo service apache2 reload
