0f0a50741d32f4535ae2aa516b2793e3aa04be9d
dhanlaxmihackosphere <74894198+dhanlaxmihackosphere@users.noreply.github.com>
2020-12-29 13:21:35 +0530

    edited

--- mysql.sh
+++ mysql.sh
@@ -6,7 +6,7 @@
 sudo apt-get -y install mysql-server
 
 #mysql secure installation - sudo mysql_secure_installation
-sudo mysql -u root <<-EOF
+sudo mysql -u root << EOF
 UPDATE mysql.user SET authentication_string = Password('$rootpass'), WHERE User='root';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE User='';
