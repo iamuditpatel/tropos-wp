6e976a7612579288df019545646c8db30fede7b0
dhanlaxmihackosphere <74894198+dhanlaxmihackosphere@users.noreply.github.com>
2020-12-29 20:12:57 +0530

    edited

--- wp.sh
+++ wp.sh
@@ -15,11 +15,11 @@ sudo tar xzvf latest.tar.gz
 chown -R www-data:www-data /var/www/wordpress/
 
 #Database Creation
-sudo mysql -u root -p '$rootpass' -Bse 'Show databases; 
+sudo mysql -u root -p '$rootpass' -Bse "Show databases; 
 CREATE DATABASE '$dbname'; 
 Show databases; 
 CREATE USER '$dbuser'@'localhost' IDENTIFIED WITH mysql_native_password BY '$dbpass'; 
-GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES;'
+GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
 #Changing Directory
 #cd /var/www/wordpress
@@ -63,7 +63,8 @@ fct_update_salts() {
 fct_update_salts $SITE_PATH
 
 #inserting in index.php
-sudo sed -i '13 a $DB_NAME = "'$dbname'";\n $DB_HOST = "'localhost'";\n $DB_USER = "'$dbuser'";\n $DB_PASSWORD = "'$dbpass'";\n $conn = mysqli_connect($DB_NAME, $DB_HOST, $DB_USER, $DB_PASSWORD); ' /var/www/wordpress/index.php
+sudo sed -i '13 a $DB_NAME = "'$dbname'";\n $DB_HOST = "'localhost'";\n $DB_USER = "'$dbuser'";\n $DB_PASSWORD = "'$dbpass'";' /var/www/wordpress/index.php
+sudo sed -i '18 a $CONN = mysqli_connect("$DB_NAME", "$DB_HOST", "$DB_USER", "$DB_PASSWORD");' /var/www/wordpress/index.php
 
 #moving all files from wordpress to your domain folder
 sudo mv /var/www/wordpress/* /var/www/$your_domain/
