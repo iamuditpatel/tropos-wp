7178ccd39b6ae7efeb222a7710ae55ddc1eeb738
dhanlaxmihackosphere <74894198+dhanlaxmihackosphere@users.noreply.github.com>
2020-12-29 12:53:34 +0530

    Added php myadmin

--- php.sh
+++ php.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-sudo apt-get -y install python-software-properties 
+#PHP Install
+sudo apt-get -y update && sudo apt-get -y install php libapache2-mod-php php-mysql
 
-sudo add-apt-repository ppa:ondrej/php -y
+#PHP MyAdmin Installation
+sudo apt-get -y update && sudo apt-get -y install phpmyadmin php-mbstring
 
-sudo apt-get -y update 
+sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
 
-sudo apt-get -y install php7.0
+sudo a2enconf phpmyadmin.conf
 
-sudo apt-get -y install php7.0-mysql php7.0-curl php7.0-json php7.0-cgi php7.0-xsl
-
-apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-mbstring php7.0-gd php7.0-xml php7.0-xmlrpc php7.0-intl php7.0-soap php7.0-zip
-
-sudo apt-get -y install php libapache2-mod-php php-mysql
+sudo service apache2 reload
