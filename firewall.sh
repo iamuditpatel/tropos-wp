a62b96aa3a2ad951e4c53f798090e08d51082f87
dhanlaxmihackosphere <74894198+dhanlaxmihackosphere@users.noreply.github.com>
2020-12-29 12:45:38 +0530

    Added Apache Full

--- firewall.sh
+++ firewall.sh
@@ -7,6 +7,9 @@ sudo ufw app list
 #To allow Apache ufw
 sudo ufw allow in "Apache"
 
+#To allow Apache Full ufw
+sudo ufw allow in "Apache Full"
+
 #If Ufw is disable then to enable it
 sudo ufw enable
 
