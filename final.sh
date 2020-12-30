cedaee42309f1d81f887d77187c3c050ca9a0ae4
dhanlaxmihackosphere <74894198+dhanlaxmihackosphere@users.noreply.github.com>
2020-12-30 20:56:07 +0530

    edited

--- final.sh
+++ final.sh
@@ -18,8 +18,8 @@ source ./apache.sh
 
 #MYsql Password Generator
 PASSWORD=$(date +%s|sha256sum|base64|head -c 12)
-LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 12
-rootpass="${PASSWORD}${LC_ALL}"
+#LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 12
+rootpass="${PASSWORD}"
 
 #Mysql Installation
 source ./mysql.sh
@@ -34,14 +34,14 @@ source ./virtualhost.sh
 dbname=`hexdump -n 8 -v -e '/1 "%02X"' /dev/urandom`
 
 #Database User Generator
-user='dbuser'
+user='user'
 pass=`hexdump -n 8 -v -e '/1 "%02X"' /dev/urandom`
 dbuser="${user}${pass}"
 
 #Database Paswword Generator
 PASSWORD=$(date +%s|sha256sum|base64|head -c 12)
-LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 12
-dbpass="${PASSWORD}${LC_ALL}"
+#LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 12
+dbpass="${PASSWORD}"
 
 #Wordpress Installation
 source ./wp.sh
