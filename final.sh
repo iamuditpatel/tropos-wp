97297a91f43d9929489cb3fcebeca6c380d94e24
dhanlaxmihackosphere <74894198+dhanlaxmihackosphere@users.noreply.github.com>
2020-12-30 13:38:07 +0530

    edited

--- final.sh
+++ final.sh
@@ -19,7 +19,7 @@ source ./firewall.sh
 #MYsql Password Generator
 PASSWORD=$(date +%s|sha256sum|base64|head -c 12)
 LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 12
-rootpass="${PASSWORD} ${LC_ALL}"
+rootpass="${PASSWORD}${LC_ALL}"
 
 #Mysql Installation
 source ./mysql.sh
@@ -41,7 +41,7 @@ dbuser="${user}${pass}"
 #Database Paswword Generator
 PASSWORD=$(date +%s|sha256sum|base64|head -c 12)
 LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 12
-dbpass="${PASSWORD} ${LC_ALL}"
+dbpass="${PASSWORD}${LC_ALL}"
 
 #Wordpress Installation
 source ./wp.sh
