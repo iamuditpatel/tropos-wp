1b584677aa26f9eb398516b6de76d7d4ca7db04d
Udit Patel <admin@uditpatel.com>
2024-11-02 18:04:42 +0530

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-#PHP Install
-sudo apt-get -y update && sudo apt-get -y install php libapache2-mod-php php-mysql
-
-#PHP MyAdmin Installation
-#sudo apt-get -y update && sudo apt-get -y install phpmyadmin php-mbstring
-
-#sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
-
-#sudo a2enconf phpmyadmin.conf
-
-#sudo service apache2 reload
+# Install PHP and necessary modules
+sudo apt-get -y install php libapache2-mod-php php-mysql
