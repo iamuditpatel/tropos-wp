2d6328427615bf41e908ec5f216795493f128966
Udit Patel <admin@uditpatel.com>
2024-11-02 18:20:22 +0530

    Update virtualhost.sh

--- virtualhost.sh
+++ virtualhost.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
+# Configure Virtual Host
+sudo tee /etc/apache2/sites-available/$your_domain.conf > /dev/null <<EOF
+<VirtualHost *:80>
+    ServerAdmin webmaster@$your_domain
+    ServerName $your_domain
+    DocumentRoot /var/www/$your_domain
 
-#Creat a directory of your domain
-sudo mkdir /var/www/$your_domain/
+    ErrorLog ${APACHE_LOG_DIR}/$your_domain-error.log
+    CustomLog ${APACHE_LOG_DIR}/$your_domain-access.log combined
 
-#Get owner Permission
-sudo chown -R $USER:$USER /var/www/$your_domain/
+    <Directory /var/www/$your_domain/>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
 
-#Copying the virtual host content to your_domain.conf file
-sudo cp ./virtual_host.txt  /etc/apache2/sites-available/$your_domain.conf
-
-#Replacing your domain in .conf file
-sudo sed -i "s/domain/$your_domain/g" /etc/apache2/sites-available/$your_domain.conf
-
-#To enable rewrite module
-sudo a2enmod rewrite
-
-#To enable new virtual host
-sudo a2ensite $your_domain
-
-#Disable the default conf
-sudo a2dissite 000-default
-
-#To check if any error exist in configuration file
-sudo apache2ctl configtest
-
-#Reload the Apache Server
-sudo systemctl reload apache2
+</VirtualHost>
+EOF
