2ad3b124529458f6b348ff654f936727ecb1ee9e
Udit Patel <admin@uditpatel.com>
2024-11-02 18:04:12 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -3,21 +3,14 @@
 
 
 #***************MYSQL INSTALLATION***************
+# Install MySQL and secure installation
 sudo apt-get -y install mysql-server
+sudo mysql_secure_installation << EOF
 
-#mysql secure installation - sudo mysql_secure_installation
-sudo mysql -u root << EOF
-UPDATE mysql.user SET authentication_string ='$rootpass' WHERE User='root';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
-FLUSH PRIVILEGES;
+$rootpass
+$rootpass
+n
+n
+n
+n
 EOF
-
-#sed command to add default_authentication_plugin
-sudo sed -i '28 a default_authentication_plugin= mysql_native_password' /etc/mysql/mysql.conf.d/mysqld.cnf
-
-#To start, enable and check the status of Mysql
-sudo systemctl start mysql
-sudo systemctl enable mysql
